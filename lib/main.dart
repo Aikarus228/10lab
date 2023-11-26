7f31d443305c0782b8319b6bdb92a6d1ca4800de
Aikarus228 <matgvo123@gmail.com>
2023-11-26 16:15:17 +0300

    2

--- lib/main.dart
+++ lib/main.dart
@@ -8,72 +8,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: UserProfileScreen(),
+      home: ProductDetailScreen(),
     );
   }
 }
 
-class UserProfileScreen extends StatelessWidget {
+class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Личный кабинет'),
+        title: Text('Подробная информация о товаре'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('assets/user_avatar.jpg'), // Замените путь на свое изображение
+            Image.asset(
+              'assets/cosmetic_product.jpg', // Замените путь на свою картинку товара
+              height: 200,
+              fit: BoxFit.cover,
             ),
-            SizedBox(height: 16),
-            Text(
-              'Иван Иванов',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'ivan@example.com',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 16),
-            Card(
-              elevation: 4,
-              child: ListTile(
-                leading: Icon(Icons.person),
-                title: Text('Личные данные'),
-                onTap: () {
-                  // Обработка нажатия
-                },
-              ),
-            ),
-            Card(
-              elevation: 4,
-              child: ListTile(
-                leading: Icon(Icons.security),
-                title: Text('Безопасность'),
-                onTap: () {
-                  // Обработка нажатия
-                },
-              ),
-            ),
-            Card(
-              elevation: 4,
-              child: ListTile(
-                leading: Icon(Icons.exit_to_app),
-                title: Text('Выйти'),
-                onTap: () {
-                  // Обработка нажатия
-                },
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Название товара',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Описание товара и другая дополнительная информация.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Цена: \$19.99',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Добавьте обработчик нажатия для добавления товара в корзину или другую логику
+                    },
+                    child: Text('Добавить в корзину'),
+                  ),
+                ],
               ),
             ),
           ],
